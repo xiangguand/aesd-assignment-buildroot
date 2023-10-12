d562e33ae79e6769c838a4e8ccb82f5d6bdeef7d
Danny Deng <zxpay73945788@gmail.com>
2023-10-12 22:40:36 +0800

    update aesd assignments mk file

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -10,7 +10,7 @@ AESD_ASSIGNMENTS_VERSION = '96785cd8143d5905c7692b8b06864736c1178619'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:ZXPAY/aesd-assignment-danny.git'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:ZXPAY/aesd-assignment-4part2.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
@@ -20,7 +20,7 @@ endef
 
 ### add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	echo "HHHHHHHHHHHHHHHHHHHH"
+	echo "AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS"
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
