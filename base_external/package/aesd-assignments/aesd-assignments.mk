1ece6946d259abe12c730c3c44477a2c9d5277ea
Danny Deng <zxpay73945788@gmail.com>
2023-10-15 14:30:06 +0800

    imple assignment5

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 ### Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '136c562b67df9a8767aecd992af8e3cac53fd797'
+AESD_ASSIGNMENTS_VERSION = '7fad072310c712f1b9005613bde4e9f45bc4c8a5'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
