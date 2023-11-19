139f77cd8aecd4c5675224882143a6884db9ab2f
Danny Deng <zxpay73945788@gmail.com>
2023-11-19 15:56:10 +0800

    full test pass

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,12 +6,14 @@
 ##############################################################
 
 ### Fill up the contents below in order to reference your assignment 3 git contents
-LDD_VERSION = '54f89566ee4d71921b60677adc6f10e37051e3d2'
+LDD_VERSION = 'e37c6d49228ed2b680ab60a6e07a0e26859de6e9'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 # LDD_VERSION = 1.0.0
 LDD_SITE = 'git@github.com:ZXPAY/aesd-ldd3.git'
+
+
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
@@ -19,17 +21,17 @@ LDD_GIT_SUBMODULES = YES
 
 # LDD_SOURCE = $(BR2_EXTERNAL_Assignment7_PATH)/../ldd3
 
-LDD_MODULE_SUBDIRS = $(@D)
-LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+LDD_MODULE_SUBDIRS = misc-modules scull
+# LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
-define LDD_BUILD_CMDS
-	echo $(@D)
-	$(MAKE) -C $(@D)
-endef
+# define LDD_BUILD_CMDS
+# 	echo $(@D)
+# 	$(MAKE) -C $(@D)
+# endef
 
-define LDD_INSTALL_TARGET_CMDS
+# define LDD_INSTALL_TARGET_CMDS
 #	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) INSTALL_MOD_PATH=$(TARGET_DIR) modules_install
-endef
+# endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
